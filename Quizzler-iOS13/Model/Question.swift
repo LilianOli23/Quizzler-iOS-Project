595acfa182e871c8c1f7518863b4e6cd344795a4
Lilian de Oliveira Silva <86902837+LilianOli23@users.noreply.github.com>
2022-04-11 20:59:57 -0300

    Add files via upload

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Lilian De Oliveira Silva on 11/04/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String){
+        text = q
+        answer = a
+    }
+}
+
+
+
+
+
